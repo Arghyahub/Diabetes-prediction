8f03bca39b393b101a729f07ded8cd75f8751670
Arghya <arghyadas242004@gmail.com>
2024-01-11 17:27:19 +0530

    update: CORS access

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 from flask import Flask , request , jsonify
+from flask_cors import CORS
 
 from controller.ann import Predict
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def hello_world():
